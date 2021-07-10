1de53f3731e52d37831e4b0b63b26d6cf86aefb1
OksanaFilevska <79194604+OksanaFilevska@users.noreply.github.com>
2021-07-10 05:38:56 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,8 +5,11 @@ const modalEl = document.querySelector('.js-lightbox');
 const modalOverlay = document.querySelector('.lightbox__overlay');
 const modalImg = document.querySelector('.lightbox__image');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+const body = document.querySelector('body');
 
 const cardsMarkup = makeGalleryMarkup(galleryItems);
+
+let currentIndex = 0;
 
 galleryList.insertAdjacentHTML('beforeend', cardsMarkup);
 
@@ -15,8 +18,9 @@ function makeGalleryMarkup(images) {
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
-    <img      
-      class="gallery__image"
+    <img
+      loading="lazy"
+      class="gallery__image lazyload"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
@@ -29,8 +33,9 @@ function makeGalleryMarkup(images) {
 }
 
 galleryList.addEventListener('click', onCardClick);
-modalOverlay.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', onOverlay);
 closeModalBtn.addEventListener('click', closeModal);
+// modalEl.addEventListener('click', closeModal);
 
 function onCardClick(e) {
   e.preventDefault();
@@ -39,30 +44,47 @@ function onCardClick(e) {
     return;
   }
 
+  //   console.log(e.target.dataset.source);
+  //     openModal(e.target.dataset.source);
   openModal(e);
 }
 
 function openModal(e) {
   modalEl.classList.add('is-open');
+  body.classList.add('is-open');
 
   modalImg.src = e.target.dataset.source;
   modalImg.alt = e.target.alt;
+
+  galleryItems.forEach(el => {
+    if (el.original === modalImg.src) {
+      currentIndex = galleryItems.indexOf(el);
+    }
+  });
 
   window.addEventListener('keydown', onListenerButton);
 }
 
 function closeModal() {
   modalEl.classList.remove('is-open');
+  body.classList.remove('is-open');
   modalImg.src = '';
-  modalImg.alt = '';
-  window.removeEventListener('keydown', onListenerButton);
+}
+
+function onOverlay(e) {
   if (e.target !== modalOverlay) {
     return;
   }
+
+  closeModal();
 }
+
+// window.addEventListener('keydown', onListenerButton);
 
 function onListenerButton(e) {
   const ESCAPE = 'Escape';
+  const ARROWRIGHT = 'ArrowRight';
+  const ARROWLEFT = 'ArrowLeft';
   const isArrowRight = e.code === 'ArrowRight';
   const isArrowLeft = e.code === 'ArrowLeft';
 
@@ -70,16 +92,15 @@ function onListenerButton(e) {
   if (e.code === ESCAPE) {
     closeModal();
   }
-
-  if (isArrowRight || isArrowLeft) {
+  if (ARROWRIGHT || ARROWLEFT) {
     showNextImg(isArrowRight);
   }
 }
 
-const imgArr = galleryItems.map(img => img.original);
+const imglArr = galleryItems.map(img => img.original);
 function showNextImg(direction) {
   let index;
-  let currentImg = imgArr.indexOf(modalImg.src);
+  let currentImg = imglArr.indexOf(modalImg.src);
   index = direction ? currentImg + 1 : currentImg - 1;
   if (index < 0) {
     index = galleryItems.length + index;
@@ -87,5 +108,5 @@ function showNextImg(direction) {
   if (index === galleryItems.length) {
     index = 0;
   }
-  modalImg.src = imgArr[index];
+  modalImg.src = imglArr[index];
 }
